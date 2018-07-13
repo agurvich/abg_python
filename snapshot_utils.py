7d030eb4a2db8ee8022ddd26343b1be600e91d67
Alex Gurvich <agurvich@u.northwestern.edu>
2018-07-13 15:30:31 -0500

    added chimes support to opensnapshot

--- snapshot_utils.py
+++ snapshot_utils.py
@@ -202,8 +202,11 @@ def get_fnames(snapdir,snapnum):
     if len(fnames) > 1:
         raise Exception("Too many files found for that snapnum!",fnames)
 
-    if os.path.isdir(fnames[0]):
-        fnames = [os.path.join(fnames[0],fname) for fname in os.listdir(fnames[0])]
+    try:
+	if os.path.isdir(fnames[0]):
+	    fnames = [os.path.join(fnames[0],fname) for fname in os.listdir(fnames[0])]
+    except IndexError:
+	raise IOError("Snapshot %d not found in %s"%(snapnum,snapdir))
     
     return fnames
 
@@ -231,9 +234,21 @@ def openSnapshot(
     snapshot_name='snapshot', extension='.hdf5',
     cosmological=0,header_only=0,
     keys_to_extract = None,
-    fnames = None):
+    fnames = None,
+    chimes_keys = []):
 
+    ## get filenames of the snapshot in question
     fnames = get_fnames(snapdir,snapnum) if fnames is None else fnames
+
+    ## split off chimes keys, if necessary
+    if keys_to_extract is not None:
+	popped = 0
+	for i,key in enumerate(keys_to_extract):
+	    ## if the key was put into keys_to_extract instead of chimes_key
+	    ##	which is a uSER ERROR(!!)
+	    if key in chimes_dict:
+		## transfer the key to the chimes_keys, WHERE IT BELONGS
+		chimes_keys+=[keys_to_extract.pop(i-popped)]
     
     new_dictionary = {}
     
@@ -272,6 +287,13 @@ def openSnapshot(
 				value = np.array(handle['PartType%d/%s'%(ptype,pkey)])*unit_fact
 
                             new_dictionary[pkey] = value
+
+		if ( (ptype == 0) and 
+		     ('ChimesAbundances' in handle['PartType0'].keys())):
+		    for chimes_species in chimes_keys:
+			chimes_index = chimes_dict[chimes_species] 
+			new_dictionary[chimes_species+'Abundance']=np.array(
+			    handle['PartType0/ChimesAbundances'][:,chimes_index])
             else:
                 if not header_only:
                     ## append particle array for each file
@@ -335,3 +357,62 @@ def readsnapToDF(snapdir,snapnum,parttype):
     
     snap_df = pd.DataFrame(res,index=ids)
     return snap_df
+
+## thanks Alex Richings!
+def read_chimes(filename, chimes_species): 
+    ''' filename - string containing the name of the HDF5 file to read in. 
+        chimes_species - string giving the name of the ion/molecule to extract.''' 
+
+    h5file = h5py.File(filename, "r") 
+
+    try: 
+        chimes_index = chimes_dict[chimes_species] 
+    except KeyError: 
+        print "Error: species %s is not recognised in the CHIMES abundance array. Aborting." % (chimes_species, ) 
+        return 
+
+    output_array = h5file["PartType0/ChimesAbundances"][:, chimes_index] 
+    h5file.close() 
+
+    return output_array 
+
+chimes_dict = {"elec": 0,
+               "HI": 1,"HII": 2,"Hm": 3,"HeI": 4,
+               "HeII": 5,"HeIII": 6,"CI": 7,"CII": 8,
+               "CIII": 9,"CIV": 10,"CV": 11,"CVI": 12,
+               "CVII": 13,"Cm": 14,"NI": 15,"NII": 16,
+               "NIII": 17,"NIV": 18,"NV": 19,"NVI": 20,
+               "NVII": 21,"NVIII": 22,"OI": 23,"OII": 24,
+               "OIII": 25,"OIV": 26,"OV": 27,"OVI": 28,
+               "OVII": 29,"OVIII": 30,"OIX": 31,"Om": 32,
+               "NeI": 33,"NeII": 34,"NeIII": 35,"NeIV": 36,
+               "NeV": 37,"NeVI": 38,"NeVII": 39,"NeVIII": 40,
+               "NeIX": 41,"NeX": 42,"NeXI": 43,"MgI": 44,
+               "MgII": 45,"MgIII": 46,"MgIV": 47,"MgV": 48,
+               "MgVI": 49,"MgVII": 50,"MgVIII": 51,"MgIX": 52,
+               "MgX": 53,"MgXI": 54,"MgXII": 55,"MgXIII": 56,
+               "SiI": 57,"SiII": 58,"SiIII": 59,"SiIV": 60,
+               "SiV": 61,"SiVI": 62,"SiVII": 63,"SiVIII": 64,
+               "SiIX": 65,"SiX": 66,"SiXI": 67,"SiXII": 68,
+               "SiXIII": 69,"SiXIV": 70,"SiXV": 71,"SI": 72,
+               "SII": 73,"SIII": 74,"SIV": 75,"SV": 76,
+               "SVI": 77,"SVII": 78,"SVIII": 79,"SIX": 80,
+               "SX": 81,"SXI": 82,"SXII": 83,"SXIII": 84,
+               "SXIV": 85,"SXV": 86,"SXVI": 87,"SXVII": 88,
+               "CaI": 89,"CaII": 90,"CaIII": 91,"CaIV": 92,
+               "CaV": 93,"CaVI": 94,"CaVII": 95,"CaVIII": 96,
+               "CaIX": 97,"CaX": 98,"CaXI": 99,"CaXII": 100,
+               "CaXIII": 101,"CaXIV": 102,"CaXV": 103,"CaXVI": 104,
+               "CaXVII": 105,"CaXVIII": 106,"CaXIX": 107,"CaXX": 108,
+               "CaXXI": 109,"FeI": 110,"FeII": 111,"FeIII": 112,
+               "FeIV": 113,"FeV": 114,"FeVI": 115,"FeVII": 116,
+               "FeVIII": 117,"FeIX": 118,"FeX": 119,"FeXI": 120,
+               "FeXII": 121,"FeXIII": 122,"FeXIV": 123,"FeXV": 124,
+               "FeXVI": 125,"FeXVII": 126,"FeXVIII": 127,"FeXIX": 128,
+               "FeXX": 129,"FeXXI": 130,"FeXXII": 131,"FeXXIII": 132,
+               "FeXXIV": 133,"FeXXV": 134,"FeXXVI": 135,"FeXXVII": 136,
+               "H2": 137,"H2p": 138,"H3p": 139,"OH": 140,
+               "H2O": 141,"C2": 142,"O2": 143,"HCOp": 144,
+               "CH": 145,"CH2": 146,"CH3p": 147,"CO": 148,
+               "CHp": 149,"CH2p": 150,"OHp": 151,"H2Op": 152,
+               "H3Op": 153,"COp": 154,"HOCp": 155,"O2p": 156}
