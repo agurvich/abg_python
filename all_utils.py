68d0397243c73fd67bb5dfd72d8243a10a7c18ce
Alex Gurvich <agurvich@u.northwestern.edu>
2020-07-20 22:15:26 -0500

    add more flexibility to add docstring appending

--- all_utils.py
+++ all_utils.py
@@ -42,12 +42,22 @@ def filter_kwargs(func,kwargs):
             bad[arg] = kwargs[arg]
     return good,bad 
 
-def append_function_docstring(function_1,function_2):
+def append_function_docstring(function_1,function_2,**kwargs):
     #print(function_1,function_2)
     #print(function_1.__doc__,function_2.__doc__)
-    function_1.__doc__+="\n--------\n %s:\n %s"%(
-        function_2.__name__,
+    function_1.__doc__ = append_string_docstring(
+        function_1.__doc__,function_2,**kwargs)
+
+def append_string_docstring(string,function_2,use_md=True,prepend_string=''):
+    prepend_string += use_md*"### "
+    name = function_2.__qualname__
+    if use_md:
+        name = '`' + name + '`' 
+
+    string+="\n--------\n %s:\n %s"%(
+        prepend_string + name,
         function_2.__doc__)
+    return string
 
     
 def get_size(obj, seen=None):
