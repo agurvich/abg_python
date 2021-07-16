acc2b108c2eb3a7995e2d48b9575fbe467722ffb
Alex Gurvich <agurvich@u.northwestern.edu>
2021-07-15 23:02:28 -0500

    update selection utils to be sleeker

--- abg_python/selection_utils.py
+++ abg_python/selection_utils.py
@@ -1,18 +1,25 @@
-def extractRectangularVolumeIndices(rs,rcom,radius,height):
-   x_indices = (rs-rcom)[:,0]**2<radius**2
-   y_indices = (rs-rcom)[:,1]**2<radius**2
+import numpy as np
 
-   height = radius if height==0 else height
-   z_indices = (rs-rcom)[:,2]**2<height**2
-   return np.logical_and(np.logical_and(x_indices,y_indices),z_indices)
+def rectangularVolumeMask(coords,radius,height=None,rcom=None):
+    if rcom is None: rcom = np.zeros(3)
+    if height is None: height = radius  
 
-def extractCylindricalVolumeIndices(coords,r,h,rcom=None):
-    if rcom is None:
-        rcom = np.array([0,0,0])
-    gindices = np.sum((coords[:,:2])**2.,axis=1) < r**2.
-    gzindices = (coords[:,2])**2. < h**2.
-    indices = np.logical_and(gindices,gzindices)
-    return indices
+    r2s = (coords-rcom)**2
+    x_indices = r2s[:,0]<=radius**2
+    y_indices = r2s[:,1]<=radius**2
 
-def extractSphericalVolumeIndices(rs,rcom,radius):
-    return np.sum((rs - rcom)**2.,axis=1) < radius**2
+    z_indices = r2s[:,2]<=height**2
+    return np.logical_and(np.logical_and(x_indices,y_indices),z_indices)
+
+def cylindricalVolumeMask(coords,radius,height=None,rcom=None):
+    if rcom is None: rcom = np.zeros(3)
+    if height is None: height = radius  
+
+    r2s = (coords-rcom)**2
+    xy_indices = np.sum(r2s[:,:2],axis=1) <= radius**2.
+    zindices = r2s[:,2] <= height**2.
+    return np.logical_and(xy_indices,zindices)
+
+def sphericalVolumeMask(coords,radius,rcom=None):
+    if rcom is None: rcom = np.zeros(3)
+    return np.sum((coords - rcom)**2.,axis=1) <= radius**2
