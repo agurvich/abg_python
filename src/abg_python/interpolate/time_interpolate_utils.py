0323741825f1b0d7fa8269781e2037b57227e626
Alex Gurvich <agurvich@u.northwestern.edu>
2021-12-19 12:48:12 -0600

    lookup star particle progenitors for young stars, alternatively, interpolate star particles backwards (doesn't work that well), use velocity correction to minimize absolute velocity difference (according to Phil's comments) switch from vbar -> this_first_vels in approx_radians because next/first sign changes can mess things up

--- src/abg_python/interpolate/time_interpolate_utils.py
+++ src/abg_python/interpolate/time_interpolate_utils.py
@@ -1,3 +1,5 @@
+import copy
+
 import numpy as np
 import pandas as pd
 
@@ -6,6 +8,7 @@ from ..galaxy.gal_utils import Galaxy
 from ..array_utils import filterDictionary
 from ..math_utils import getThetasTaitBryan, rotateEuler
 from ..math_utils import get_cylindrical_velocities,get_cylindrical_coordinates,get_spherical_coordinates,get_spherical_velocities
+from ..physics_utils import get_IMass
 
 def find_bordering_snapnums(
     snap_times_gyr,
@@ -44,7 +47,8 @@ def index_match_snapshots_with_dataframes(
     extra_arrays_function=None,
     t0=None,
     t1=None,
-    coord_interp_mode='spherical'):
+    coord_interp_mode='spherical',
+    extra_df=None):
     """
         if you use Metallicity  or Velocities then the keys will be different when you try to access them
           in your render call using render_kwargs.
@@ -61,7 +65,15 @@ def index_match_snapshots_with_dataframes(
             " arbitrary quantity arrays from stuff computed in the snapshot.")
 
     if keys_to_extract is None: keys_to_extract = []
-    keys_to_extract += ['Coordinates','Masses','SmoothingLength','ParticleIDs','ParticleChildIDsNumber']
+    keys_to_extract += [
+        'Coordinates',
+        'Masses',
+        'SmoothingLength',
+        'Velocities',
+        'ParticleIDs',
+        'ParticleChildIDsNumber',
+        'AgeGyr',
+        'Temperature']
 
     pandas_kwargs = dict(
         keys_to_extract=keys_to_extract,
@@ -79,33 +91,103 @@ def index_match_snapshots_with_dataframes(
     next_df_snap_reduced = next_df_snap.reindex(prev_df_snap.index,copy=False)
 
     ## add back stars that formed between snapshots
-    if 'AgeGyr' in keys_to_extract:
+    if 'AgeGyr' in keys_to_extract and 'AgeGyr' in next_sub_snap:
         ## find stars w/ age in last snapshot < snapshot spacing
         new_star_mask = next_sub_snap['AgeGyr'] < (t1-t0)
         next_young_star_snap = filterDictionary(next_sub_snap,new_star_mask)
-        next_young_star_snap['AgeGyr'] = t0 - next_young_star_snap['AgeGyr']
+
         next_young_star_df = convertSnapToDF(next_young_star_snap,**pandas_kwargs)
 
+        ## now we have to initialize the young stars in the prev_snap
+        prev_young_star_snap = {}#copy.copy(next_young_star_snap)
+        ## set their ages to be negative
+
+        if extra_df is not None:
+            prev_young_star_snap['AgeGyr'] = next_young_star_df['AgeGyr'] - (t1-t0)
+            next_stars_with_parents_mask = next_young_star_df.index.isin(extra_df.index)
+            next_young_star_df = next_young_star_df[next_stars_with_parents_mask]
+            gas_parent_mask = extra_df.index.isin(next_young_star_df.index)
+
+            ## filter and then reorder
+            gas_parent_df = extra_df.loc[gas_parent_mask].loc[next_young_star_df.index]
+
+            for key in next_young_star_df.keys():
+                if key == 'AgeGyr': continue
+                prev_young_star_snap[key] = gas_parent_df[key]
+
+            prev_young_star_df = pd.DataFrame(
+                prev_young_star_snap,
+                index=next_young_star_df.index)
+
+        else:
+            ## copy the velocities, assume they have constant velocity from birth I guess
+            for key in ['ParticleChildIDsNumber','ParticleIDs','Velocities','Metallicity','SmoothingLength']:
+                prev_young_star_snap[key] = next_young_star_snap[key]
+
+            prev_young_star_snap['AgeGyr'] = next_young_star_snap['AgeGyr'] - (t1-t0)
+
+            ## account for mass loss-- this will be linearly interpolated between
+            ##  later on (rather than applying the mass loss factor as should happen
+            ##  but that's okay for now, it's close enough.
+            prev_young_star_snap['Masses'] = get_IMass(
+                next_young_star_snap['AgeGyr'],
+                next_young_star_snap['Masses'])
+            
+            ## figure out what coordinates they had at birth. best we can do is interpolate backwards
+            ## (we could look for a matching gas particle but there's no guarantee that DF was loaded
+            ## :\. it also seems way more complex to handle than just interpolating backwards)
+            this_coords = next_young_star_snap['Coordinates']
+            this_vels = next_young_star_snap['Velocities']
+
+            ## find angular momentum frame for each particle
+            Ls = np.cross(this_coords,this_vels*next_young_star_snap['Masses'][:,None])
+
+            ## get rotation matrices
+            theta_TBs,phi_TBs = getThetasTaitBryan(Ls.T)
+            rot_matrices = rotateEuler(theta_TBs,phi_TBs,0,None,loud=False)
+            ## reshape to be Npart x 3 x 3
+            rot_matrices = np.rollaxis(rot_matrices,-1,0)
+
+            ## rotate into L frame, still in cartesian coordinates
+            this_coords = (rot_matrices*this_coords[:,None]).sum(-1)
+            this_vels = (rot_matrices*this_vels[:,None]).sum(-1)
+            ## did we request spherical coordinates?
+            if coord_interp_mode == 'spherical':
+                this_vels = np.array(get_spherical_velocities(this_vels,this_coords),order='C').T
+                this_coords = np.array(get_spherical_coordinates(this_coords),order='C').T
+            ## did we request cylindrical coordinates?
+            elif coord_interp_mode == 'cylindrical':
+                this_vels = np.array(get_cylindrical_velocities(this_vels,this_coords),order='C').T
+                this_coords = np.array(get_cylindrical_coordinates(this_coords),order='C').T
+
+            prev_young_star_snap['Coordinates'] = (
+                np.transpose(rot_matrices,axes=(0,2,1))* ## rotate out of L frame
+                interpolate_coords( ## interpolate in spherical/cylindrical coordinates *and* convert to cartesian
+                    this_coords,
+                    this_vels,
+                    None,
+                    None,
+                    t0,t1,t0,
+                    coord_interp_mode=coord_interp_mode)[:,None]
+                ).sum(-1) 
+
+            prev_young_star_df = convertSnapToDF(prev_young_star_snap,**pandas_kwargs)
+
+        ## append the young stars to each dataframe
         next_df_snap_reduced = next_df_snap_reduced.append(next_young_star_df)
+        prev_df_snap = prev_df_snap.append(prev_young_star_df)
+
+        ## and for good measure let's re-sort now that we 
+        ##  added new indices into the mix
         next_df_snap_reduced.sort_index(inplace=True)
+        prev_df_snap.sort_index(inplace=True)
 
-    ## merge rows of dataframes based on 
-    """
-    lol this used to work, obviously, but now it
-    raises an internal error when trying to extract the
-    multi-indices. pandas sucks.
-
-    prev_next_merged_df_snap = pd.merge(
-        prev_df_snap,
-        next_df_snap_reduced,
-        how='inner',
-        on=prev_df_snap.index,
-        suffixes=('','_next'),
-        copy=False).set_index('key_0')
-    """
+    ## merge rows of dataframes based on particle ID
     prev_next_merged_df_snap = prev_df_snap.join(
         next_df_snap_reduced,
         rsuffix='_next')
+    prev_next_merged_df_snap = prev_next_merged_df_snap.loc[
+        ~prev_next_merged_df_snap.index.duplicated(keep='first')]
 
     ## remove particles that do not exist in the next snapshot, 
     ##  difficult to tell if they turned into stars or were merged. 
@@ -180,9 +262,10 @@ def index_match_snapshots_with_dataframes(
                     this_vels,
                     this_coords)
 
+        new_df = pd.DataFrame(copy_snap,index=prev_next_merged_df_snap.index)
         ## append these columns to the dataframe
-        prev_next_merged_df_snap = prev_next_merged_df_snap.join(
-            pd.DataFrame(copy_snap,index=prev_next_merged_df_snap.index))
+        prev_next_merged_df_snap = prev_next_merged_df_snap.join(new_df)
+        #for key in copy_snap.keys(): print(key,copy_snap[key].shape)
                 
         ## we'll need the inverse rotation matrices
         ##  later, so let's store them as the inverse (transpose)
@@ -275,10 +358,10 @@ def make_interpolated_snap(
     interp_snap['Coordinates'] = coords
     interp_snap['Velocities'] = vels
 
-    ## remove stars that will form between this and the next snapshot
-    ##  but haven't formed yet
-    ## TODO what is the AgeGyr value for stars that haven't formed w/o the _next prefix??
-    if 'AgeGyr' in interp_snap: interp_snap = filterDictionary(interp_snap,interp_snap['AgeGyr']>0)
+    ## remove stars that have not formed yet
+    if 'AgeGyr' in interp_snap: 
+        age_mask = np.logical_and(interp_snap['AgeGyr']>0,interp_snap['AgeGyr']<0.025)
+        interp_snap = filterDictionary(interp_snap,age_mask)
  
     return interp_snap
 
@@ -299,32 +382,52 @@ def interpolate_coords(
     
     for i in range(3):
         this_first_coords = first_coords[:,i]
-        this_next_coords = next_coords[:,i]
-
         this_first_vels = first_vels[:,i]
-        this_next_vels = next_vels[:,i]
-        
+
+        if next_vels is not None: this_next_vels = next_vels[:,i]
+        else: this_next_vels = first_vels[:,i]
+
         vbar = (this_first_vels+this_next_vels)/2.
 
+        ## handle angular stuff
+        if coord_interp_mode == 'spherical':
+            phi_index = 2
+            ## divide vels by r3d, for theta
+            if i == 1: 
+                renorm = rtp_coords[:,0]
+                vbar = vbar/renorm
+                this_first_vels = this_first_vels/renorm
+                this_next_vels = this_next_vels/renorm
+            ## divide vels by r2d, for phi
+            elif i ==2: 
+                renorm = (rtp_coords[:,0]*np.sin(rtp_coords[:,1]))
+                vbar = vbar/renorm
+                this_first_vels = this_first_vels/renorm
+                this_next_vels = this_next_vels/renorm
+        elif coord_interp_mode == 'cylindrical':
+            phi_index = 1
+            ## divide vels by r2d, for phi
+            if i == 1: 
+                renorm = rtp_coords[:,0]
+                vbar = vbar/renorm
+                this_first_vels = this_first_vels/renorm
+                this_next_vels = this_next_vels/renorm
+        else: phi_index = None
+
+        if next_coords is not None: this_next_coords = next_coords[:,i]
+        ## handle extrapolation case
+        else: 
+            if phi_index is None or i != phi_index:
+                this_next_coords = this_first_coords + this_first_vels*dsnap
+            else:
+                this_next_coords = this_first_coords + np.mod(this_first_vels*dsnap,2*np.pi)
         
         dcoord = this_next_coords - this_first_coords
 
-        ## r theta phi
-        if coord_interp_mode == 'spherical':
-            phi_index = 2
-            r2d = rtp_coords[:,0]*np.sin(rtp_coords[:,1])
-        elif coord_interp_mode == 'cylindrical':
-            phi_index = 1
-            r2d = rtp_coords[:,0]
-        else:
-            phi_index = None
-
         if i == phi_index: 
-            ## relevant angular velocity is v/r2d
-            vbar = vbar/r2d
-
             ## how far would we guess each particle could go?
-            approx_radians = vbar*dsnap
+            #approx_radians = vbar*dsnap ## phil suggests errors can happen if v changes sign
+            approx_radians = this_first_vels*dsnap
 
             ## let's guess how many times it went around, basically
             ##  want to determine which of (N)*2pi + dcoord  
@@ -332,7 +435,11 @@ def interpolate_coords(
             ##  closest to approx_radians, (N = approx_radians//2pi)
             dcoord = guess_windings(dcoord,approx_radians,2*np.pi)
 
-        rtp_coords[:,i] = (this_first_coords + dcoord*time_frac)
+        rtp_coords[:,i] = (
+            ## basic linear interpolation
+            this_first_coords + dcoord*time_frac +  
+            ## correction factor to minimize velocity difference, apparently
+            (this_next_vels - this_first_vels)/2*dsnap * time_frac * (time_frac - 1)) 
 
     ## cast spherical coordinates to cartesian coordinates
     if coord_interp_mode=='spherical': 
@@ -359,9 +466,13 @@ def guess_windings(dphi,vphi_dt,period=1):
     base_windings = vphi_dt//period
     ## now we have to decide, do we go back a whole winding
     ##  to match dphi or do we go forward part of a winding?
-    guesses = (base_windings+np.array([-1,0,1])[:,None])*period+(dphi-vphi_dt)
+    guesses = (base_windings+np.array([-1,0,1])[:,None])*period + dphi
     
-    ## take advantage of the fact that options are -1,0,1, turn corresponding indices from 0,1,2 -> -1,0,1
-    adjust_windings = np.argmin(np.abs(guesses),axis=0)-1
+    ## find the guess that is closest to the predicted vphi_dt
+    adjust_windings = np.argmin(np.abs(guesses-vphi_dt),axis=0)
+
+    ## take advantage of the fact that options are -1,0,1 to 
+    ## turn min index from 0,1,2 -> -1,0,1
+    adjust_windings-=1
 
     return (base_windings+adjust_windings)*period+dphi
